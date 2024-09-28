1ce6bfe888d700246db3fd31305a4e89fd26a3b9
serversaravanam22 <serversaravanam22@gmail.com>
2024-09-28 07:23:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ from lazybot.clients import initialize_clients
 
 PORT = "8080"
 LazyPrincessBot.start()
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
 
 
 async def Lazy_start():
